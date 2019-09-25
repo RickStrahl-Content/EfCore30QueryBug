50f38a485db4ecf15e0e53068992a356c0acfe06
rickstrahl <rstrahl@west-wind.com>
2019-09-24 21:30:02 -0700

    Add project files.

---
+++ EfCoreQueryBug.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlbumViewerBusiness;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCoreQueryBug
+{
+    [TestClass]
+    public class EfCoreQueryBug
+    {
+        [TestMethod]
+        public void EfCoreQueryWithSqLiteDataBug()
+        {
+            var Context = new AlbumViewerContext() {useSqLite = true};
+
+
+            IQueryable<Album> albums = Context.Albums
+                .Include(ctx => ctx.Tracks)
+                .Include(ctx => ctx.Artist)
+                .OrderBy(alb => alb.Title);
+
+
+            var list = albums.ToList();
+
+            var listUninitialized = list.Where(a => a.Artist.Id == 0).ToList();
+
+            Assert.IsTrue(list.Count > 80, "Total number of records should be greater than 80");
+            Assert.IsTrue(listUninitialized.Count == 0,
+                $"There are {listUninitialized.Count} uninitialized Artist records");
+        }
+
+        [TestMethod]
+        public void ImportAlbumData()
+        {
+            var Context = new AlbumViewerContext()
+            {
+                useSqLite = false,
+                ConnectionString = "server=.;database=AlbumViewer2;integrated security=true;MultipleActiveResultSets=true;App=AlbumViewer"
+            };
+            AlbumViewerDataImporter.EnsureAlbumData(Context, Path.GetFullPath("./albums.js"));
+
+        }
+    }
+}
---
+++ Models/Context/AlbumViewerContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+
+namespace AlbumViewerBusiness
+{
+    public class AlbumViewerContext : DbContext
+    {
+        public string ConnectionString { get; set; } =
+            "server=.;database=AlbumViewer4;integrated security=true;MultipleActiveResultSets=true;App=AlbumViewer";
+
+        public bool useSqLite = true;
+
+        public AlbumViewerContext(DbContextOptions options) : base(options)
+        {         
+            
+        }
+
+        public AlbumViewerContext()
+        { }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<User> Users { get; set;  }
+
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {         
+            base.OnModelCreating(builder);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (optionsBuilder.IsConfigured)
+                return;
+
+            if (!useSqLite)
+                optionsBuilder.UseSqlServer(ConnectionString);
+            else
+            {
+                // Note this path has to have full  access for the Web user in order
+                // to create the DB and write to it.
+                var connStr = "Data Source=" +
+                              Path.Combine(".\\AlbumViewerData.sqlite");
+                optionsBuilder.UseSqlite(connStr);
+            }
+        }
+
+    }
+}
